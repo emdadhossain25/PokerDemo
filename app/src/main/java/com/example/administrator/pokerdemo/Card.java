fc3c165f36f23415a9dfcfde389ec9dd74db3d3a
emdad.hossain25 <emdad.hossain321@gmail.com>
2017-04-08 23:09:40 +0600

    added some logic for setting face value ace,king,queen and jack

--- app/src/main/java/com/example/administrator/pokerdemo/Card.java
+++ app/src/main/java/com/example/administrator/pokerdemo/Card.java
@@ -7,28 +7,48 @@ package com.example.administrator.pokerdemo;
 public class Card {
     private String face;
     private String suit;
+
     //two-argument constructor initializes Cards face and suit
     public Card(String face, String suit) {
         super();
-        this.face = face;
+        if (face.equalsIgnoreCase("14")) {
+            this.face = "Ace";
+        }
+        if (face.equalsIgnoreCase("13")) {
+            this.face = "King";
+        }
+        if (face.equalsIgnoreCase("12")) {
+            this.face = "Queen";
+        }
+        if (face.equalsIgnoreCase("11")) {
+            this.face = "Jack";
+        }
+        else {
+            this.face = face;
+        }
         this.suit = suit;
     }
+
     //getter method to return the face value
     public String getFace() {
         return face;
     }
+
     //setter method to initialize the face
     public void setFace(String face) {
         this.face = face;
     }
+
     //getter method to return the suit value
     public String getSuit() {
         return suit;
     }
+
     //setter method to initialize the suit
     public void setSuit(String suit) {
         this.suit = suit;
     }
+
     //return String representation of Card object
     public String toString() {
         return face + " of " + suit;
