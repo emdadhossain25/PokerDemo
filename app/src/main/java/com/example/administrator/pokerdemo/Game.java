05ad72cc1e6e6d7504516c6f1f2db2061af67ee4
emdad.hossain25 <emdad.hossain321@gmail.com>
2017-04-10 20:07:58 +0600

    correcting  a typo

--- app/src/main/java/com/example/administrator/pokerdemo/Game.java
+++ app/src/main/java/com/example/administrator/pokerdemo/Game.java
@@ -13,16 +13,18 @@ public class Game {
     private Player[] players;
     private Deck deck;
     Context context;
+
     //constructor initializes the deck and cards
     public Game(Context context, int deal_hands) {
-        this.context=context;
+        this.context = context;
         deck = new Deck(context);
         players = new Player[deal_hands];
-        for (int i =0; i<deal_hands;i++){
-            players[i]= new Player();
+        for (int i = 0; i < deal_hands; i++) {
+            players[i] = new Player();
         }
         deck.shuffle(0);
     }
+
     //deals the card to each player
     public void dealCards() {
         int count = 0;
@@ -32,86 +34,57 @@ public class Game {
             }
         }
     }
+
     //simulates the game and shows the result
     public void showCards() {
-        TextView txtView = (TextView) ((MainActivity)context).findViewById(R.id.deal_text);
+        TextView txtView = (TextView) ((MainActivity) context).findViewById(R.id.deal_text);
         txtView.setText(null);
         txtView.setMovementMethod(new ScrollingMovementMethod());
-        txtView.scrollTo(0,0);
+        txtView.scrollTo(0, 0);
 
         for (int i = 0; i < players.length; i++) {
             txtView.append("Player " + (i + 1) + ": ");
             txtView.append("\n");
             txtView.append("-------------------------");
             for (int j = 0; j < players[0].getCards().length; j++) {
-                txtView.append("\ncard :"+" "+ players[i].getCardAtIndex(j).toString()+"\n ");
+                txtView.append("\ncard :" + " " + players[i].getCardAtIndex(j).toString() + "\n ");
             }
 
-            if(players[i].royalFlush()==1){
+            if (players[i].royalFlush() == 1) {
                 txtView.append("\nResult: Royal Flush");
-            }
-            else if(players[i].straightFlush()==1)
-            {
+            } else if (players[i].straightFlush() == 1) {
                 txtView.append("\nResult: FLUSH!!\n");
-            }
-
-            else if(players[i].fourOfaKind()==1)
-            {
+            } else if (players[i].fourOfaKind() == 1) {
                 txtView.append("\nResult: Four of a kind!!\n");
-            }
-
-             if(players[i].fullHouse()==1)
-            {
+            } else if (players[i].fullHouse() == 1) {
                 txtView.append("\nResult: Full House!!\n");
-            }
-
-            else if(players[i].flush()==1)
-            {
+            } else if (players[i].flush() == 1) {
                 txtView.append("\nResult: Flush!!\n");
-            }
-
-            else if(players[i].straight()==1)
-            {
+            } else if (players[i].straight() == 1) {
                 txtView.append("\nResult: Straight!!\n");
-            }
-
-            else if(players[i].threeofakind()==1)
-            {
+            } else if (players[i].threeofakind() == 1) {
                 txtView.append("\nResult: Three of a kind!!\n");
-            }
-
-            else if(players[i].twoPairs()==1)
-            {
+            } else if (players[i].twoPairs() == 1) {
                 txtView.append("\nResult: Two pairs!!\n");
-            }
-
-            else if(players[i].pair()==1)
-            {
+            } else if (players[i].pair() == 1) {
                 txtView.append("\nResult: Pair!!\n");
-            }
-
-            else if(players[i].highCard()!=0)
-            {
-                if (players[i].highCard()==14){
-                    txtView.append("\nHighest Card:"+" Ace");
-                }
-                else if (players[i].highCard()==13){
-                    txtView.append("\nHighest Card:"+" King");
-                }
-                else if (players[i].highCard()==12){
-                    txtView.append("\nHighest Card:"+" Queen");
-                }
-                else if (players[i].highCard()==11){
-                    txtView.append("\nHighest Card:"+" Jack");
-                }
-                else
-                txtView.append("\nHighest Card: "+players[i].highCard());
+            } else if (players[i].highCard() != 0) {
+                if (players[i].highCard() == 14) {
+                    txtView.append("\nHighest Card:" + " Ace");
+                } else if (players[i].highCard() == 13) {
+                    txtView.append("\nHighest Card:" + " King");
+                } else if (players[i].highCard() == 12) {
+                    txtView.append("\nHighest Card:" + " Queen");
+                } else if (players[i].highCard() == 11) {
+                    txtView.append("\nHighest Card:" + " Jack");
+                } else
+                    txtView.append("\nHighest Card: " + players[i].highCard());
             }
 
 
             txtView.append("\n");
             txtView.append("\n");
         }
-        Toast.makeText(context,"Deal! Please scroll text view to see full result",Toast.LENGTH_LONG).show();
+        Toast.makeText(context, "Deal! Please scroll text view to see full result", Toast.LENGTH_LONG).show();
     }
 }
